70ee32771ab04d7fe116bc4db8113d4a0f1061a5
elizabeth-chavol <elizabeth.chavol.infosec@gmail.com>
2026-07-15 16:31:32 -0300

    Clase 6: Detector con log de evidencias y alertas por correo

--- detector_alertas.py
+++ detector_alertas.py
@@ -3,11 +3,20 @@ import time
 from dotenv import load_dotenv
 import smtplib
 from email.message import EmailMessage
+from datetime import datetime
 
 load_dotenv()
 EMAIL = os.getenv("EMAIL")
 PASSWORD = os.getenv("PASSWORD")
 LOG_FILE = "logs/ataques.log"
+
+def guardar_log(ip, intentos):
+    fecha = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    linea =  f"[{fecha}] ALERTA: {intentos} intentos fallidos desde IP {ip}\n"
+
+    with open("alertas.log", "a") as f:
+        f.write(linea)
+    print(f"Evidencia guardada en alertas.log")
 
 def enviar_alerta(mensaje):
     msg = EmailMessage()
@@ -27,6 +36,7 @@ def vigilar():
         with open(LOG_FILE, 'r') as f:
             contenido = f.read()
             if "ATAQUE" in contenido or "ERROR" in contenido:
+                guardar_log("LOGSERVER", 5)
                 enviar_alerta(f"Se detectó esto en el log:\n{contenido}")
         time.sleep(60) # espera 60 seg
 
